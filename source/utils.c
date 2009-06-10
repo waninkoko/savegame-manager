f017b6357f77b7482ac6dee56dc99f465ff8dadc
Waninkoko <waninkoko@gmail.com>
2009-06-10 21:43:10 +0200

    Fix StrToHex64(). Thanks to tueidj.

--- source/utils.c
+++ source/utils.c
@@ -16,7 +16,7 @@ u64 StrToHex64(const char *str)
 		u32  idx = len - (cnt + 1);
 		char c   = toupper(str[idx]);
 
-		u64 n = (isdigit(c)) ? c - '0' : c - 'A';
+		u64 n = (isdigit(c)) ? c - '0' : (c - 'A') + 0xA;
 		u64 m = 1;
 
 		for (idx = 0; idx < cnt; idx++)
